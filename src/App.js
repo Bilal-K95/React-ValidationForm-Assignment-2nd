937d6da0f6ffa445bfff614cda05c89cd166d823
Bilal-K95 <bilal.khatik@neosoftmail.com>
2021-10-14 16:48:49 +0530

    React validation form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import Registration from "./Component/Registration";
+// import Form from "./Component/Form";
+
+function App() {
+  return (
+    
+      <Registration />
+    
+  );
+}
+
+export default App;
